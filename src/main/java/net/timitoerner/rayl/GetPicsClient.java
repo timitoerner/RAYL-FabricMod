140824367a46a30f9f5cadc94486e17ee4723adc
timitoerner <tim@kultpower.de>
2023-06-18 20:53:52 +0200

    Initial commit

---
+++ src/main/java/net/timitoerner/rayl/GetPicsClient.java
@@ -0,0 +1,10 @@
+package net.timitoerner.rayl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GetPicsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
